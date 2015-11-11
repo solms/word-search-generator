87a53686844db94a733077278720e36266264012
Stykes <solmsd@gmail.com>
2015-11-11 18:09:25 +0900

    Added alphabet, started with logic

---
+++ js/custom-script.js
@@ -0,0 +1,33 @@
+var words = [];
+
+$(document).ready(function(){
+	$('#entered-words').hide();
+
+	$('#get-words #submit').on('click', function(){
+		var word = $('#get-words #word').val().toUpperCase();
+		if(words.indexOf(word) == -1){
+			words.push(word);
+			$('#get-words #word').val('');
+			refreshWordsList();
+		}
+		else{
+			alert("You've already entered that word!");
+		}		
+	});
+
+	$('#get-words #finished').on('click', function(){
+		generateWordSearch();
+	});
+});
+
+function refreshWordsList(){
+	var display_list = "";
+	for(var i=0; i<words.length; i++)
+		display_list += "<li>"+words[i]+"</li>";
+	$('#entered-words #words-list').html(display_list);
+	$('#entered-words').show('fast');
+}
+
+function generateWordSearch(){
+	
+}
