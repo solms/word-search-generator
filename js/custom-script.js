a0ad8564c98522e56a928df897ea5d8f961b5b03
Stykes <solmsd@gmail.com>
2015-11-11 18:25:53 +0900

    Busy with word placement

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,5 +1,9 @@
-var alphabet = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+var alphabet = ['A','B','C','D','E','F','G','H','I',
+				'J','K','L','M','N','O','P','Q','R',
+				'S','T','U','V','W','X','Y','Z'];
 var words = [];
+var n = 100;
+var word_search = [];
 
 $(document).ready(function(){
 	$('#entered-words').hide();
@@ -30,5 +34,40 @@ function refreshWordsList(){
 }
 
 function generateWordSearch(){
+	// Initialise an nxn array with ''s
+	var row = [];
+	for(var i=0; i<n; i++)
+		row.push('');
+	for(var i=0; i<n; i++)
+		word_search.push(row);
+	// Place each word
+	for(var i=0; i<words.length; i++)
+		placeWord(words[i]);
+}
 
+function placeWord(word){
+	// Guess a random starting position at least
+	// word.length from a side
+	var l = word.length;
+	var starting_pos = [
+		Math.floor(Math.random()*(n-2*l))+l,
+		Math.floor(Math.random()*(n-2*l))+l
+	];
+
+	// Choose a random direction:
+	// 0: left to right
+	// 1: top to bottom
+	var direction = Math.floor(Math.random()*2);
+
+	// Check if there are any letters in the way in this direction
+	var obstruction = false;
+	var current = '';
+	for(var i=0; i<l; i++){
+		if(direction == 0)
+			current = word_search[starting_pos[0]][starting_pos[1]+i];
+		else
+			current = word_search[starting_pos[0]+i][starting_pos[1]];
+		if(current != '')
+		obstruction = true;			
+	}
 }
